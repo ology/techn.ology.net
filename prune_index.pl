e35670f4f1f55048af712645dde73873a1b02eb5
Gene Boggs <gene@ology.net>
2020-12-01 10:42:36 -0800

    Make the marker a variable

--- prune_index.pl
+++ prune_index.pl
@@ -5,6 +5,9 @@ use warnings;
 use File::Copy qw(move);
 use YAML::XS qw(LoadFile);
 
+# This divides the beginning of the post from the "meat"
+my $marker = qr|<p>~</p>|;
+
 my $conf = LoadFile('site.yml');
 
 my $source = "$conf->{deploy}{path}/index.html";
@@ -16,7 +19,7 @@ open(my $out, '>', $dest)
     or die "Can't write $dest: $!";
 
 while (my $line = readline($in)) {
-    if ($line =~ m|<p>~</p>|) {
+    if ($line =~ $marker) {
         while (my $temp = readline($in)) {
             if ($temp =~ m|</article>|) {
                 print $out $temp;
@@ -25,7 +28,7 @@ while (my $line = readline($in)) {
         }
     }
     print $out $line
-        unless $line =~ m|<p>~</p>|;
+        unless $line =~ $marker;
 }
 
 close $in
