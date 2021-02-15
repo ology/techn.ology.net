09923c122168f05acdfac6b1460b1154357b624a
Gene Boggs <gene@ology.net>
2021-02-15 06:45:53 -0800

    Fix comment

--- webgui.pl
+++ webgui.pl
@@ -82,7 +82,7 @@ sub titleize {
 sub slugize {
   my ($title) = @_;
   (my $slug = $title) =~ s/\s+/-/g;
-  $slug =~  s/(?!-)[[:punct:]]//g; # Remove all punctuation besides the hyphen
+  $slug =~ s/(?!-)[[:punct:]]//g; # Remove all punctuation except the hyphen
   $slug = lc $slug;
   return $slug;
 }
